612a88d06752ce2de1417fd5a0808cb3b62c5a6b
PG <spg2616@gmail.com>
2023-03-14 10:08:56 -0300

    finish

--- Manager/src/Manager.API/Program.cs
+++ Manager/src/Manager.API/Program.cs
@@ -12,6 +12,7 @@ using Manager.Services.Interfaces;
 using Manager.Services.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -70,7 +71,42 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Manager API",
+        Version = "v1",
+        Description = "API construída na serie de vídeos no canal Lucas Eschechola.",
+        Contact = new OpenApiContact
+        {
+            Name = "Lucas Eschechola",
+            Email = "lucas.gabriel@eu.com",
+            Url = new Uri("https://eschechola.com.br")
+        },
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Por favor utilize Bearer <TOKEN>",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
